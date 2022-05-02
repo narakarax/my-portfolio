15919d01f462b5f830f089e247195aa66e2ede14
nerona001 <aaronchupa610@gmail.com>
2022-05-01 20:20:05 -0700

    change

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -7,7 +7,7 @@ var items = [
     <SlideImgItem imgURL={"/assets/img/portfolio/mnting_nft.png"} imgLink={"https://nftest-sandy.vercel.app/"} skillContent={"React + React Hook + TypeScript + Node.js + TailwindCSS + SCSS + GraphQL"} key="" />,
     <SlideImgItem imgURL={"/assets/img/portfolio/hapeio.png"} imgLink={"https://hape.io/"} skillContent={"React + Solidity + Node.js + Three.js + Express.js + MongoDB"} key="2" />,
     <SlideImgItem imgURL={"/assets/img/portfolio/First page.png"} imgLink={"http://gamesite-eight.vercel.app"} skillContent={"Three.js + WebGL"} key="3" />,
-    <SlideImgItem imgURL={"/assets/img/portfolio/WebVR-ShowRoom.png"} imgLink={"https://webvr-showroom.vercel.app/"} skillContent={"Web Virtual Reality : VR + WebGL + Next.js + TypeScript + Node.js"} key="4" />,
+    <SlideImgItem imgURL={"/assets/img/portfolio/Image 14.png"} imgLink={"https://museudastoninhas.com.br/"} skillContent={"Web Virtual Reality : VR + WebGL + Next.js + TypeScript + Node.js"} key="4" />,
     <SlideImgItem imgURL={"/assets/img/portfolio/nftmarket place.png"} imgLink={"https://exoworlds.io/"} skillContent={"Blockchain site : Smart contract + Next.js + TypeScript + Node.js"} key="5" />,
     <SlideImgItem imgURL={"/assets/img/portfolio/First.png"} imgLink={"https://droppgroup.com/"} skillContent={"Company site : Rewact + D3.js + Typescript + Node.js +"} key="6" />,
 
