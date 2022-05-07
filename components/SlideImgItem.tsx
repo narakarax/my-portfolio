73349264f7dc1d95d5064e95338ddf38ffb1d8e2
narakarax <liliyaogerenko@gmail.com>
2022-05-07 08:34:21 -0700

    main

--- components/SlideImgItem.tsx
+++ components/SlideImgItem.tsx
@@ -7,7 +7,7 @@ export default function SlideImgItem({imgURL, imgLink, skillContent}) {
       <Link href={imgLink}>
         <a target="_blank">
           <div className="relative h-full  border-4 border-white hover:shadow-lg">
-            <div className="relative w-full h-48 md:h-64 hover:cursor-pointer">
+            <div className="relative w-full h-64 md:h-80 hover:cursor-pointer">
               {
                 (imgURL==='my-portfolio')?
                 <iframe src={imgLink} className="w-full h-full shadown-md "/>
