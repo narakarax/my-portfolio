8322fa9d4d0c8a3fcdb55c88d7a9a8e4b36101d5
narakarax <liliyaogerenko@gmail.com>
2022-05-09 06:45:35 -0700

    main

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -4,7 +4,11 @@ import ReactPaginate from 'react-paginate';
 import SlideImgItem from './SlideImgItem';
 // Example items, to simulate fetching from another resources.
 var items = [  
+  <SlideImgItem imgURL={"/assets/img/portfolio/mnting_nft.png"} imgLink={"https://nftest-sandy.vercel.app/"} skillContent={"React + React Hook + TypeScript + Node.js + TailwindCSS + SCSS + GraphQL"} key="4" />,
   <SlideImgItem imgURL={"/assets/img/portfolio/nissan.png"} imgLink={"https://www.nissanusa.com/"} skillContent={"Next.js + tailwind CSS + Node.js + Postgres"} key="1" />,
+  <SlideImgItem imgURL={"/assets/img/portfolio/First.png"} imgLink={"https://droppgroup.com/"} skillContent={"Company site : Rewact + D3.js + Typescript + Node.js +"} key="11" />,
+  <SlideImgItem imgURL={"/assets/img/portfolio/nftmarket place.png"} imgLink={"https://exoworlds.io/"} skillContent={"Blockchain site : Smart contract + Next.js + TypeScript + Node.js"} key="10" />,
+  <SlideImgItem imgURL={"/assets/img/portfolio/hapeio.png"} imgLink={"https://hape.io/"} skillContent={"React + Solidity + Node.js + Three.js + Express.js + MongoDB"} key="12" />,
   // <SlideImgItem imgURL={"/assets/img/portfolio/nissan.png"} imgLink={"https://www.nissanusa.com/"} skillContent={"Next.js + tailwind CSS + Node.js + Postgres"} key="1" />,
   <SlideImgItem imgURL={"/assets/img/portfolio/wedding.png"} imgLink={"https://www.unforgettableproposals.com.au/"} skillContent={"React + Node.js + API"} key="8" />,
   <SlideImgItem imgURL={"/assets/img/portfolio/human.png"} imgLink={"https://www.greenwoodcampbell.com/"} skillContent={".NET + anime.js + Bootstrap + GSAP"} key="2" />,
@@ -13,12 +17,8 @@ var items = [
   <SlideImgItem imgURL={"/assets/img/portfolio/constructor.png"} imgLink={"https://www.fieldfocus.com/"} skillContent={"React + SCSS + Node.js+ Express.js + MongoDB"} key="6" />,
   <SlideImgItem imgURL={"/assets/img/portfolio/Australlia.png"} imgLink={"https://immi.homeaffairs.gov.au/"} skillContent={"Angular + Bootstrap + Express.js + MongoDB"} key="5" />,
   <SlideImgItem imgURL={"/assets/img/portfolio/Angular.png"} imgLink={"https://summerofcode.withgoogle.com/"} skillContent={"Angular + tailwind CSS"} key="3" />,
-  <SlideImgItem imgURL={"/assets/img/portfolio/mnting_nft.png"} imgLink={"https://nftest-sandy.vercel.app/"} skillContent={"React + React Hook + TypeScript + Node.js + TailwindCSS + SCSS + GraphQL"} key="4" />,
   <SlideImgItem imgURL={"/assets/img/portfolio/Game.png"} imgLink={"http://gamesite-eight.vercel.app"} skillContent={"React + tailwind CSS + Figma"} key="7" />,
   <SlideImgItem imgURL={"/assets/img/portfolio/Vue.png"} imgLink={"https://www.hovedkontor.no/"} skillContent={"Vue Project : Vue.js + Tailwind CSS"} key="9" />,
-  <SlideImgItem imgURL={"/assets/img/portfolio/nftmarket place.png"} imgLink={"https://exoworlds.io/"} skillContent={"Blockchain site : Smart contract + Next.js + TypeScript + Node.js"} key="10" />,
-  <SlideImgItem imgURL={"/assets/img/portfolio/First.png"} imgLink={"https://droppgroup.com/"} skillContent={"Company site : Rewact + D3.js + Typescript + Node.js +"} key="11" />,
-  <SlideImgItem imgURL={"/assets/img/portfolio/hapeio.png"} imgLink={"https://hape.io/"} skillContent={"React + Solidity + Node.js + Three.js + Express.js + MongoDB"} key="12" />,
   ];                
 function Items({ currentItems }) {
   return (
